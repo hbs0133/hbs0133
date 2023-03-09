48142b5aafdf8bb165b56950a75a44d7e72cd380
hbs0133 <126546310+hbs0133@users.noreply.github.com>
2023-03-09 23:35:19 +0900

    Delete memo directory

--- memo/onepage/script/script.js
+++
@@ -1,6 +0,0 @@
-$(function() {
-  $('nav a').click(function(e) {
-    $.scrollTo(this.hash || 0, 850);
-    e.preventDeafult();
-  });
-});
--- memo/rush/script/script.js
+++
@@ -1,6 +0,0 @@
-$(function() {
-  $('#wrap #nav #nav_left a').click(function(e) {
-    $.scrollTo(this.hash || 0, 850);
-    e.preventDeafult();
-  });
-});
