2e653a15d7df902b6ac5dd0874f8e473701dd122
hbs0133 <126546310+hbs0133@users.noreply.github.com>
2023-03-16 18:27:42 +0900

    Add files via upload

---
+++ memo/MobileWebPage/js/script.js
@@ -0,0 +1,14 @@
+$(function() {
+  $('.gnb-include').load('/include/gnb.html');
+  $('.main-header-include').load('/include/main-header.html');
+
+  /*Category Accordien*/
+  $('.category-accordion .detail').eq(0).show()
+  $('.category-accordion .title').click(function () {
+    $(this).next().stop().slideDown();
+    $(this).siblings('.category-accordion .title').next().stop().slideUp();
+
+    $(this).addClass('active')
+    $(this).siblings('category-accordion .title').removeClass('active');
+  });
+});
