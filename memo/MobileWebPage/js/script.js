938ff8142604868f515024a81530652f0ba7e57a
hbs0133 <126546310+hbs0133@users.noreply.github.com>
2023-03-17 18:21:56 +0900

    Add files via upload

--- memo/MobileWebPage/js/script.js
+++ memo/MobileWebPage/js/script.js
@@ -8,7 +8,34 @@ $(function() {
     $(this).next().stop().slideDown();
     $(this).siblings('.category-accordion .title').next().stop().slideUp();
 
-    $(this).addClass('active')
+    $(this).addClass('active');
     $(this).siblings('category-accordion .title').removeClass('active');
   });
+
+  /* Recent Search Result */
+  $('.search-recent .btn-clear').click(function() {
+    $(this).parent().hide();
+  });
+  $('.search-recent .btn-allclear').click(function() {
+    $('.search-recent .item').hide();
+  });
+
+  /* Cart Items */
+  $('.cart-items-header .btn-allclear').click(function() {
+    $('.cart-item').hide();
+  });
+  $('.cart-item-title .btn-clear').click(function() {
+    $(this).parent().parent().hide();
+  });
+
+  /* Front Siler */
+  $('.front-slider').slick({
+    slidesToShow: 1,
+    dots: false,
+    arrows: true,
+    autoplay: false,
+    autoplaySpeed: 3000,
+    slidesToScroll: 1
+  });
+
 });
