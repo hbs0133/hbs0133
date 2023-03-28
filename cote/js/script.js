c4b326fe036e1e33ccafed2c2da20c24007ca8d0
hbs0133 <126546310+hbs0133@users.noreply.github.com>
2023-03-28 18:27:11 +0900

    Add files via upload

---
+++ cote/js/script.js
@@ -0,0 +1,11 @@
+// $('.my-img').slick({
+//   infinite: true,
+//   slidesToShow: 1,
+//   slidesToScroll: 1
+// });
+$('.my-img').slick({
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 1000,
+});
